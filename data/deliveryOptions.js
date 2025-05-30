e6256e3d39346bfa8e2188899e2de20601e68167
McJuma <smarkjuma8@gmail.com>
2025-05-30 21:34:10 +0300

    Add delivery options and update cart summary in checkout

---
+++ data/deliveryOptions.js
@@ -0,0 +1,19 @@
+export const deliveryOptions = [
+    {
+        id: "1",
+        deliveryPeriod: 7,
+        costInCents: 0
+    },
+
+    {
+        id: "2",
+        deliveryPeriod: 3,
+        costInCents: 499
+    },
+
+    {
+        id: "3",
+        deliveryPeriod: 1,
+        costInCents: 999
+    }
+];
