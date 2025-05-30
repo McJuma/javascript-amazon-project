56f212de824b66ae2ada7275294991cdb8de40b5
McJuma <smarkjuma8@gmail.com>
2025-05-30 16:46:30 +0300

    Refactor cart management: update cart structure and implement item removal functionality in checkout

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -17,7 +17,7 @@ cart.forEach((cartItem) => {
 
 
    cartSummaryHtml += `
-   <div class="cart-item-container">
+   <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -40,7 +40,7 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-cart-item" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -96,3 +96,14 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHtml;
+
+document.querySelectorAll(".js-delete-cart-item").forEach((link) => {
+   link.addEventListener("click", () => {
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+      const cartItemContainer = document.querySelector(`.js-cart-item-container-${productId}`);
+      
+      cartItemContainer.remove();
+   });
+   
+});
